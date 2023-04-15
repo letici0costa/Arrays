d6e65b2b73b0b55dfdf716b780ca9e944b373dd4
letici0costa <109427133+letici0costa@users.noreply.github.com>
2023-04-15 11:27:33 -0300

    Add files via upload

---
+++ Atividade - array/script.js
@@ -0,0 +1,21 @@
+//ATIVIDADE
+
+//01
+//const names = ["Jéssica", "Jennifer", "Leticia"];
+//names.push("Jayne");
+
+//console.log(names);
+
+//02
+//const animal = ["Gato", "Papagaio", "Leão", "Piriquito"];
+//animal.pop();
+
+//console.log(animal);
+
+//03
+//const fruit = ["Melão", "Melancia", "Maçã"]
+//fruit.unshift("Uva");
+
+//console.log(fruit);
+
+
